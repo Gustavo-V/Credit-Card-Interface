91dd82def9ec5698f5639cf94809ccb51e2d4362
Gustavo <gustavo.forbusinesses@gmail.com>
2022-10-21 00:06:46 -0300

    setting mask to cvc and expiration date

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import Imask from "imask"
 
 class CreditCard{
     constructor(){
@@ -14,7 +15,7 @@ class CreditCard{
 
         this.flagLogo = document.querySelector(".cc-logo span:nth-child(2) img");
     }
-
+  
     setCardType(type){
         this.creditCardfills.path01.setAttribute("fill", this.colors[type][0]);
         this.creditCardfills.path02.setAttribute("fill", this.colors[type][1]);
@@ -22,5 +23,46 @@ class CreditCard{
     }
 }
 
+class MaskCreditCard{
+    constructor(){
+        this.securityCodeElement = document.querySelector("#security-code");
+        this.expirationDateElement = document.querySelector("#expiration-date");
+        this.securityCodeMasked;
+        this.expirationDateMasked;        
+    }
+
+    setSecurityCodeMask(mask){
+        const securityCodePattern = {mask: mask};
+        this.securityCodeMasked = Imask(this.securityCodeElement, securityCodePattern);
+    }
+
+    setExpirationDateMask(mask){
+        const currentYear = String(new Date().getFullYear()).slice(2);
+        const expirationYear = String(new Date().getFullYear() + 10).slice(2);
+        const expirationDatePattern = {
+            mask: mask,
+            blocks:{
+                MM:{
+                    mask: Imask.MaskedRange,
+                    from: 1,
+                    to: 12
+                },
+                YY:{
+                    mask: Imask.MaskedRange,
+                    from: currentYear,
+                    to: expirationYear
+                }
+            }            
+        }
+        this.expirationDateMasked = Imask(this.expirationDateElement, expirationDatePattern);
+    }
+
+
+}
+
 const creditCard = new CreditCard();
 creditCard.setCardType("mastercard");
+
+const maskCreditCard = new MaskCreditCard();
+maskCreditCard.setSecurityCodeMask("0000");
+maskCreditCard.setExpirationDateMask("MM{/}YY");
